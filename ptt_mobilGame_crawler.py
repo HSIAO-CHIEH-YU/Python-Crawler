b42459c6db9dfbc7567c38a4321f782a121adf20
HSIAO-CHIEH-YU <w038149@gmai..com>
2025-02-05 14:05:12 +0800

    還原點

--- ptt_mobilGame_crawler.py
+++ ptt_mobilGame_crawler.py
@@ -3,8 +3,9 @@ from os import write
 import requests
 
 url="https://www.ptt.cc/bbs/Mobile-game/index.html"
-response=requests.get(url)
-print(response.text)
+headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"}
+response=requests.get(url,headers=headers)
+#print(response.text)
 if response.status_code==200:
     with open("output.html","w",encoding="utf-8")as f:
         f.write(response.text)
