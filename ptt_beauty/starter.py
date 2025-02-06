6e1f230690936154c2c510aed1bb49811ddf5137
HSIAO-CHIEH-YU <w038149@gmai..com>
2025-02-06 12:00:07 +0800

    還原點

--- ptt_beauty/starter.py
+++ ptt_beauty/starter.py
@@ -15,7 +15,7 @@ def main():
     url="https://www.ptt.cc/bbs/Mobile-game/M.1570249701.A.877.html"
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
              "Cookie":"_gid=GA1.2.112255453.1738733955; _gat=1; _ga=GA1.1.1796970974.1738645809; cf_clearance=3mpy7SR.uu9b..s.vRTKLfiKdGRltA2zWrpXtuMLn1I-1738813319-1.2.1.1-4LRgRW_ZS1kDqAJSD7LzEpdChe0AsY5lAATUDmczVW_TQKJZX..yOASz2vnMe02RVSDjaLfV3N23sh8bBefKdr9Q0ecxjVQxKF8QxiSPJJHHRakVtAWP6I_DbhMxc9plUskhL.BUsqoiJ.dq8Hn2Fc_jQgywB2cES3RlE.emoqp1X8cqiq.Cfg8Kyv7GLjntrRe9REfGdBUXIFKEJgWp1qfRrLJfojRFlxnasI.yYsvqQEIG_7LIJf5JnmAOtYON5ib0SX5eXddU.aFkNZRUTpI4ZFJXDqvEdcszmKGPqFA; _ga_DZ6Y3BY9GW=GS1.1.1738812271.4.1.1738813323.0.0.0",
-             "Referer":"https://www.ptt.cc/"}
+             "Referer":"https://www.ptt.cc/"}#Referer假裝從ppt進入到頁面
     response=requests.get(url,headers=headers)
     soup=BeautifulSoup(response.text,"html.parser")
     #print(soup.prettify())
